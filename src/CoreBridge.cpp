3520c72b8ed2a7a419910090d988e59d5a5be194
Lu Chih-Yuan <rmrf1039@gmail.com>
2021-08-08 03:29:02 +0800

    Remove unused codes

--- src/CoreBridge.cpp
+++ src/CoreBridge.cpp
@@ -13,15 +13,16 @@ void CoreBridgeClass::end()
     SpiDrv::end();
 }
 
-int CoreBridgeClass::uartReceive(uint16_t length, uint8_t *buffer)
+int CoreBridgeClass::uartReceive(uint8_t port, uint16_t length, uint8_t *buffer)
 {
     WAIT_FOR_SLAVE_SELECT();
     // Send Command
-    SpiDrv::sendCmd(COREBRIDGE_UART_RECEIVE, PARAM_NUMS_1);
+    SpiDrv::sendCmd(COREBRIDGE_UART_RECEIVE, PARAM_NUMS_2);
+    SpiDrv::sendParam(&port, 1, NO_LAST_PARAM);
     SpiDrv::sendBufferLen16(buffer, length, LAST_PARAM);
 
     // pad to multiple of 4
-    int commandSize = 6;
+    int commandSize = 7;
     while (commandSize % 4)
     {
         SpiDrv::readChar();
@@ -54,7 +55,7 @@ char *CoreBridgeClass::uartTransmit(uint8_t &port, uint16_t &length)
     SpiDrv::spiSlaveSelect();
 
     // Wait for reply
-    static tDataParam params[PARAM_NUMS_2] = { {0, NULL},
+    tDataParam params[PARAM_NUMS_2] = { {0, NULL},
                                         {0, NULL} };
 
     SpiDrv::waitResponseDataParams(COREBRIDGE_UART_TRANSMIT, PARAM_NUMS_2, params);
@@ -67,271 +68,5 @@ char *CoreBridgeClass::uartTransmit(uint8_t &port, uint16_t &length)
     
     return params[1].data;
 }
-
-/*int CoreBridgeClass::getEnablePOP()
-{
-    WAIT_FOR_SLAVE_SELECT();
-    // Send Command
-    SpiDrv::sendCmd(COREBRIDGE_GET_ENABLE_POP, PARAM_NUMS_0);
-
-    SpiDrv::spiSlaveDeselect();
-    //Wait the reply elaboration
-    SpiDrv::waitForSlaveReady();
-    SpiDrv::spiSlaveSelect();
-
-    // Wait for reply
-    uint8_t _data = 0;
-    uint8_t _dataLen = 0;
-    SpiDrv::waitResponseCmd(COREBRIDGE_GET_ENABLE_POP, PARAM_NUMS_1, &_data, &_dataLen);
-    SpiDrv::spiSlaveDeselect();
-
-    return _data;
-}
-
-int CoreBridgeClass::createAccessory()
-{
-    return Homekit.createAccessory();
-}
-
-int CoreBridgeClass::countAccessory()
-{
-    return Homekit.countAccessory();
-}
-
-int CoreBridgeClass::beginAccessory()
-{
-    return Homekit.beginAccessory();
-}
-
-int CoreBridgeClass::addModule(uint8_t index, const char *name, uint8_t type, uint8_t priority, uint8_t state)
-{
-    WAIT_FOR_SLAVE_SELECT();
-    // Send Command
-    SpiDrv::sendCmd(COREBRIDGE_ADD_MODULE_CMD, PARAM_NUMS_5);
-    SpiDrv::sendParam(&index, 1, NO_LAST_PARAM);
-    SpiDrv::sendParam(&type, 1, NO_LAST_PARAM);
-    SpiDrv::sendParam(&priority, 1, NO_LAST_PARAM);
-    SpiDrv::sendParam(&state, 1, NO_LAST_PARAM);
-    SpiDrv::sendParam((uint8_t *)name, strlen(name), LAST_PARAM);
-
-    // pad to multiple of 4
-    int commandSize = 9 + strlen(name);
-    while (commandSize % 4)
-    {
-        SpiDrv::readChar();
-        commandSize++;
-    }
-
-    SpiDrv::spiSlaveDeselect();
-    //Wait the reply elaboration
-    SpiDrv::waitForSlaveReady();
-    SpiDrv::spiSlaveSelect();
-
-    // Wait for reply
-    uint8_t _data = 0;
-    uint8_t _dataLen = 0;
-    SpiDrv::waitResponseCmd(COREBRIDGE_ADD_MODULE_CMD, PARAM_NUMS_1, &_data, &_dataLen);
-    SpiDrv::spiSlaveDeselect();
-
-    return _data;
-}
-
-int CoreBridgeClass::getModuleSwitchState(uint8_t index)
-{
-    WAIT_FOR_SLAVE_SELECT();
-    // Send Command
-    SpiDrv::sendCmd(COREBRIDGE_GET_MODULE_SWITCH_STATE_CMD, PARAM_NUMS_1);
-    SpiDrv::sendParam(&index, 1, LAST_PARAM);
-
-    // pad to multiple of 4
-    int commandSize = 6;
-    while (commandSize % 4)
-    {
-        SpiDrv::readChar();
-        commandSize++;
-    }
-
-    SpiDrv::spiSlaveDeselect();
-    //Wait the reply elaboration
-    SpiDrv::waitForSlaveReady();
-    SpiDrv::spiSlaveSelect();
-
-    // Wait for reply
-    uint8_t _data = 0;
-    uint8_t _dataLen = 0;
-    SpiDrv::waitResponseCmd(COREBRIDGE_GET_MODULE_SWITCH_STATE_CMD, PARAM_NUMS_1, &_data, &_dataLen);
-    SpiDrv::spiSlaveDeselect();
-
-    return _data;
-}
-
-int CoreBridgeClass::setModuleSwitchState(uint8_t index, uint8_t state)
-{
-    WAIT_FOR_SLAVE_SELECT();
-    // Send Command
-    SpiDrv::sendCmd(COREBRIDGE_SET_MODULE_SWITCH_STATE_CMD, PARAM_NUMS_2);
-    SpiDrv::sendParam(&index, 1, NO_LAST_PARAM);
-    SpiDrv::sendParam(&state, 1, LAST_PARAM);
-
-    // pad to multiple of 4
-    int commandSize = 7;
-    while (commandSize % 4)
-    {
-        SpiDrv::readChar();
-        commandSize++;
-    }
-
-    SpiDrv::spiSlaveDeselect();
-    //Wait the reply elaboration
-    SpiDrv::waitForSlaveReady();
-    SpiDrv::spiSlaveSelect();
-
-    // Wait for reply
-    uint8_t _data = 0;
-    uint8_t _dataLen = 0;
-    SpiDrv::waitResponseCmd(COREBRIDGE_SET_MODULE_SWITCH_STATE_CMD, PARAM_NUMS_1, &_data, &_dataLen);
-    SpiDrv::spiSlaveDeselect();
-
-    return _data;
-}
-
-int CoreBridgeClass::setModuleCurrent(uint8_t index, uint16_t value)
-{
-    WAIT_FOR_SLAVE_SELECT();
-    // Send Command
-    SpiDrv::sendCmd(COREBRIDGE_SET_MODULE_CURRENT_CMD, PARAM_NUMS_2);
-    SpiDrv::sendParam(&index, 1, NO_LAST_PARAM);
-    SpiDrv::sendBufferLen16(&value, 1, LAST_PARAM);
-
-    // pad to multiple of 4
-    int commandSize = 9;
-    while (commandSize % 4)
-    {
-        SpiDrv::readChar();
-        commandSize++;
-    }
-
-    SpiDrv::spiSlaveDeselect();
-    //Wait the reply elaboration
-    SpiDrv::waitForSlaveReady();
-    SpiDrv::spiSlaveSelect();
-
-    // Wait for reply
-    uint8_t _data = 0;
-    uint8_t _dataLen = 0;
-    SpiDrv::waitResponseCmd(COREBRIDGE_SET_MODULE_CURRENT_CMD, PARAM_NUMS_1, &_data, &_dataLen);
-    SpiDrv::spiSlaveDeselect();
-
-    return _data;
-}
-
-int CoreBridgeClass::getModulePriority(uint8_t index)
-{
-    WAIT_FOR_SLAVE_SELECT();
-    // Send Command
-    SpiDrv::sendCmd(COREBRIDGE_GET_MODULE_PRIORITY_CMD, PARAM_NUMS_1);
-    SpiDrv::sendParam(&index, 1, LAST_PARAM);
-
-    // pad to multiple of 4
-    int commandSize = 6;
-    while (commandSize % 4)
-    {
-        SpiDrv::readChar();
-        commandSize++;
-    }
-
-    SpiDrv::spiSlaveDeselect();
-    //Wait the reply elaboration
-    SpiDrv::waitForSlaveReady();
-    SpiDrv::spiSlaveSelect();
-
-    // Wait for reply
-    uint8_t _data = 0;
-    uint8_t _dataLen = 0;
-    SpiDrv::waitResponseCmd(COREBRIDGE_GET_MODULE_PRIORITY_CMD, PARAM_NUMS_1, &_data, &_dataLen);
-    SpiDrv::spiSlaveDeselect();
-
-    return _data;
-}
-
-int CoreBridgeClass::readModuleTriggered(uint8_t index)
-{
-    WAIT_FOR_SLAVE_SELECT();
-    // Send Command
-    SpiDrv::sendCmd(COREBRIDGE_READ_MODULE_TRIGGERED_CMD, PARAM_NUMS_1);
-    SpiDrv::sendParam(&index, 1, LAST_PARAM);
-
-    // pad to multiple of 4
-    int commandSize = 6;
-    while (commandSize % 4)
-    {
-        SpiDrv::readChar();
-        commandSize++;
-    }
-
-    SpiDrv::spiSlaveDeselect();
-    //Wait the reply elaboration
-    SpiDrv::waitForSlaveReady();
-    SpiDrv::spiSlaveSelect();
-
-    // Wait for reply
-    uint8_t _data = 0;
-    uint8_t _dataLen = 0;
-    SpiDrv::waitResponseCmd(COREBRIDGE_READ_MODULE_TRIGGERED_CMD, PARAM_NUMS_1, &_data, &_dataLen);
-    SpiDrv::spiSlaveDeselect();
-
-    return _data;
-}
-
-int CoreBridgeClass::pushWarehouseBuffer(uint16_t *buffer, uint8_t length)
-{
-    WAIT_FOR_SLAVE_SELECT();
-    // Send Command
-    SpiDrv::sendCmd(COREBRIDGE_PUSH_WAREHOUSE_BUFFER, PARAM_NUMS_1);
-    SpiDrv::sendBufferLen16(buffer, length, LAST_PARAM);
-
-    // pad to multiple of 4
-    int commandSize = 6;
-    while (commandSize % 4)
-    {
-        SpiDrv::readChar();
-        commandSize++;
-    }
-
-    SpiDrv::spiSlaveDeselect();
-    //Wait the reply elaboration
-    SpiDrv::waitForSlaveReady();
-    SpiDrv::spiSlaveSelect();
-
-    // Wait for reply
-    uint8_t _data = 0;
-    uint8_t _dataLen = 0;
-    SpiDrv::waitResponseCmd(COREBRIDGE_PUSH_WAREHOUSE_BUFFER, PARAM_NUMS_1, &_data, &_dataLen);
-    SpiDrv::spiSlaveDeselect();
-
-    return _data;
-}
-
-int CoreBridgeClass::resetNetwork()
-{
-    WAIT_FOR_SLAVE_SELECT();
-    // Send Command
-    SpiDrv::sendCmd(COREBRIDGE_RESET_NETWORK_CMD, PARAM_NUMS_0);
-
-    SpiDrv::spiSlaveDeselect();
-
-    return 0;
-}
-
-int CoreBridgeClass::resetToFactory()
-{
-    WAIT_FOR_SLAVE_SELECT();
-    // Send Command
-    SpiDrv::sendCmd(COREBRIDGE_RESET_TO_FACTORY_CMD, PARAM_NUMS_0);
-
-    SpiDrv::spiSlaveDeselect();
-
-    return 0;
-}*/
 
 CoreBridgeClass CoreBridge;
--- src/CoreBridge.h
+++ src/CoreBridge.h
@@ -13,29 +13,8 @@ public:
   void begin();
   void end();
 
-  int uartReceive(uint16_t length, uint8_t *buffer);
+  int uartReceive(uint8_t port, uint16_t length, uint8_t *buffer);
   char *uartTransmit(uint8_t &port, uint16_t &length);
-
-  /*int getEnablePOP();
-
-  int createAccessory();
-  int countAccessory();
-  int beginAccessory();
-
-  int addModule(uint8_t index, const char *name, uint8_t type, uint8_t priority, uint8_t state);
-  int getModuleSwitchState(uint8_t index);
-  int setModuleSwitchState(uint8_t index, uint8_t state);
-
-  int setModuleCurrent(uint8_t index, uint16_t value);
-
-  int getModulePriority(uint8_t index);
-
-  int readModuleTriggered(uint8_t index);
-
-  int pushWarehouseBuffer(uint16_t *buffer, uint8_t length);
-
-  int resetNetwork();
-  int resetToFactory();*/
 };
 
 extern CoreBridgeClass CoreBridge;
